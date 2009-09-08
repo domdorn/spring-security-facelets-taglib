1171af2894c046ce2db0ff00fa0742ac9b17a318
Dominik Dorn <dominik@dominikdorn.com>
2009-09-08 13:59:55 +0000

    Merge branch 'jsf-2.0-spring-2' into HEAD
    git-svn-id: svn://localhost/tmp/testgc/branches/jsf-2.0-spring-2@40 c15a56d0-f83f-420d-922d-19b5f492f4ce

---
+++ taglib/src/main/java/org/springframework/security/taglibs/facelets/IfAnyGrantedTag.java
@@ -0,0 +1,52 @@
+package org.springframework.security.taglibs.facelets;
+
+import javax.el.ELException;
+import javax.faces.FacesException;
+import javax.faces.component.UIComponent;
+import javax.faces.view.facelets.*;
+import java.io.IOException;
+
+/**
+ * Taglib to combine the Spring-Security Project with Facelets <br />
+ *
+ * This is the Class responsible for making the <br />
+ * <code><br />
+ *     &lt;sec:ifAnyGranted roles=&quot;ROLE_USER,ROLE_EXAMPLE&quot;&gt;<br />
+ *         The components you want to show only when the condition holds <br />
+ *     lt;/sec:ifAnyGranted&gt;<br />
+ * </code>
+ * work.
+ *
+ *
+ * @author Dominik Dorn - http://www.dominikdorn.com/
+ * @date 2009-04-30
+ */
+public class IfAnyGrantedTag extends TagHandler
+{
+
+	private final TagAttribute roles;
+
+	public void apply(FaceletContext faceletContext, UIComponent uiComponent)
+			throws IOException, FacesException, FaceletException, ELException {
+		if(this.roles == null)
+			throw new FaceletException("roles must be given, but is null");
+
+		String roles = this.roles.getValue(faceletContext);
+		if(roles == null || roles.isEmpty())
+			throw new FaceletException("roles must be given");
+
+		if(SpringSecurityELLibrary.ifAnyGranted(roles))
+			this.nextHandler.apply(faceletContext, uiComponent);
+	}
+
+	public IfAnyGrantedTag(ComponentConfig componentConfig) {
+		super(componentConfig);	
+		this.roles = this.getRequiredAttribute("roles");
+		if(this.roles == null)
+			throw new TagAttributeException(this.roles,
+					"The `roles` attribute has to be specified!");
+
+	}
+
+
+}
