f63d6929eecf1857339ceb77263dcd9d8d1d37b8
Grzegorz Blaszczyk <grzegorz.blaszczyk@goodylabs.com>
2010-09-05 19:13:39 +0000

    CHG: testing anonymous user with more appropriate implementation of Authentication interface
    git-svn-id: svn://localhost/tmp/testgc/branches/0.4_gblaszczyk@89 c15a56d0-f83f-420d-922d-19b5f492f4ce

--- taglib-jsf12-spring-3/src/test/java/org/springframework/security/taglibs/facelets/SpringSecurityELLibraryTest.java
+++ taglib-jsf12-spring-3/src/test/java/org/springframework/security/taglibs/facelets/SpringSecurityELLibraryTest.java
@@ -1,6 +1,6 @@
 package org.springframework.security.taglibs.facelets;
 
-
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.GrantedAuthority;
@@ -9,125 +9,129 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.taglibs.facelets.SpringSecurityELLibrary;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
 import static org.mockito.Mockito.*;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
 
+import java.util.ArrayList;
+
+import org.junit.Test;
 
 /**
  * @author Dominik Dorn
  */
 public class SpringSecurityELLibraryTest {
-	private SecurityContext context;
+  private static final String KEY = "123456789";
 
+  private SecurityContext context;
 
-	private final GrantedAuthority ROLE_ADMIN = new GrantedAuthorityImpl("ROLE_ADMIN");
-	private final GrantedAuthority ROLE_USER = new GrantedAuthorityImpl("ROLE_USER");
-	private final GrantedAuthority ROLE_MODERATOR = new GrantedAuthorityImpl("ROLE_MODERATOR");
+  private final GrantedAuthority ROLE_ADMIN = new GrantedAuthorityImpl("ROLE_ADMIN");
+  private final GrantedAuthority ROLE_USER = new GrantedAuthorityImpl("ROLE_USER");
+  private final GrantedAuthority ROLE_MODERATOR = new GrantedAuthorityImpl("ROLE_MODERATOR");
 
-	@org.junit.Before
-	public void setUp() {
+  @org.junit.Before
+  public void setUp() {
 
-		context = mock(SecurityContext.class);
+    context = mock(SecurityContext.class);
 
+    SecurityContextHolder.setContext(context);
 
-		SecurityContextHolder.setContext(context);
+    User principal = new User("username", "password",
+    /* enabled */true,
+    /* accountNonExpired */true,
+    /* credentialsNonExpired */true,
+    /* accountNonLocked */true, new GrantedAuthority[] { ROLE_ADMIN, ROLE_USER });
 
-		User principal = new User("username",
-				"password",
-				/* enabled */ true,
-				/* accountNonExpired */ true,
-				/* credentialsNonExpired */ true,
-				/* accountNonLocked */ true,
-				new GrantedAuthority[]{ROLE_ADMIN, ROLE_USER});
-		
-		Authentication auth = new PreAuthenticatedAuthenticationToken(principal, new Object(), AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_ADMIN,ROLE_USER"));
-		
-		when(context.getAuthentication()).thenReturn(auth);
-	}
+    Authentication auth = new PreAuthenticatedAuthenticationToken(principal, new Object(),
+        AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_ADMIN,ROLE_USER"));
 
-	@org.junit.After
-	public void tearDown() {
-		SecurityContextHolder.clearContext();
-	}
+    when(context.getAuthentication()).thenReturn(auth);
+  }
 
-	@Test
-	public void testIfAnyGranted_noMatches()
-	{
-		assertFalse(SpringSecurityELLibrary.ifAnyGranted("ROLE_VISITOR,ROLE_MODERATOR"));
-	}
+  @org.junit.After
+  public void tearDown() {
+    SecurityContextHolder.clearContext();
+  }
 
+  @Test
+  public void testIfAnyGranted_noMatches() {
+    assertFalse(SpringSecurityELLibrary.ifAnyGranted("ROLE_VISITOR,ROLE_MODERATOR"));
+  }
 
-	@Test
-	public void testIfAnyGranted_oneMatches()
-	{
-		assertTrue(SpringSecurityELLibrary.ifAnyGranted("ROLE_VISITOR,ROLE_MODERATOR,ROLE_ADMIN"));
-	}
+  @Test
+  public void testIfAnyGranted_oneMatches() {
+    assertTrue(SpringSecurityELLibrary.ifAnyGranted("ROLE_VISITOR,ROLE_MODERATOR,ROLE_ADMIN"));
+  }
 
+  /*
+   * test if ROLE_ADMIN is not granted, as it is by our test data, this should
+   * return false
+   */
 
-	/*
-		test if ROLE_ADMIN is not granted, as it is by our test data, this should return false
-	 */
+  @Test
+  public void testIfNotGranted_roleAdmin() {
+    assertFalse(SpringSecurityELLibrary.ifNotGranted("ROLE_ADMIN"));
+  }
 
-	@Test
-	public void testIfNotGranted_roleAdmin() {
-	    assertFalse(SpringSecurityELLibrary.ifNotGranted("ROLE_ADMIN"));
-	}
+  /*
+   * test if ROLE_VISITOR is not granted, as this is not within our default test
+   * data, this should return true
+   */
+  @Test
+  public void testIfNotGranted_roleVisitor() {
+    assertTrue(SpringSecurityELLibrary.ifNotGranted("ROLE_VISITOR"));
+  }
 
-	/*
-		test if ROLE_VISITOR is not granted, as this is not within our default test data, this should return true
-	 */
-	@Test
-	public void testIfNotGranted_roleVisitor()
-	{
-		assertTrue(SpringSecurityELLibrary.ifNotGranted("ROLE_VISITOR"));
-	}
+  @Test
+  public void testIfAllGranted_notAllRolesGranted() {
+    assertFalse(SpringSecurityELLibrary.ifAllGranted("ROLE_ADMIN,ROLE_USER,ROLE_MODERATOR"));
+  }
 
-	@Test
-	public void testIfAllGranted_notAllRolesGranted() {
-		assertFalse(SpringSecurityELLibrary.ifAllGranted("ROLE_ADMIN,ROLE_USER,ROLE_MODERATOR"));
-	}
+  @Test
+  public void testIfAllGranted_allRolesGranted() {
+    assertTrue(SpringSecurityELLibrary.ifAllGranted("ROLE_ADMIN,ROLE_USER"));
+  }
 
-	@Test
-	public void testIfAllGranted_allRolesGranted() {
-		assertTrue(SpringSecurityELLibrary.ifAllGranted("ROLE_ADMIN,ROLE_USER"));
-	}
+  @Test
+  public void testIfAllGranted_noRolesSpecified() {
+    assertFalse(SpringSecurityELLibrary.ifAllGranted(""));
+  }
 
+  @Test
+  public void testIfAllGranted_nullGiven() {
+    assertFalse(SpringSecurityELLibrary.ifAllGranted(null));
+  }
 
-	@Test
-	public void testIfAllGranted_noRolesSpecified(){
-		assertFalse(SpringSecurityELLibrary.ifAllGranted(""));
-	}
+  @Test
+  public void testIsAuthenticated_authenticated() {
+    assertTrue(SpringSecurityELLibrary.isAuthenticated());
+  }
 
-	@Test
-	public void testIfAllGranted_nullGiven(){
-		assertFalse(SpringSecurityELLibrary.ifAllGranted(null));
-	}
-	
-	@Test
-	public void testIsAuthenticated_authenticated(){
-	  assertTrue(SpringSecurityELLibrary.isAuthenticated());
-	}
-	
-	@Test
-  public void testIsAuthenticated_annonymous(){
-	  Authentication anonymousAuth = new PreAuthenticatedAuthenticationToken(new Object(), new Object());
-	  when(context.getAuthentication()).thenReturn(anonymousAuth);
+  @Test
+  public void testIsAuthenticated_annonymous() {
+    Authentication anonymousAuth = new AnonymousAuthenticationToken(KEY, "anonymousUser", getAnonymousAuthorities());
+    when(context.getAuthentication()).thenReturn(anonymousAuth);
     assertFalse(SpringSecurityELLibrary.isAuthenticated());
   }
-	
-	@Test
-  public void testIsAnonymous_anonymous(){
-	  Authentication anonymousAuth = new PreAuthenticatedAuthenticationToken(new Object(), new Object());
+
+  @Test
+  public void testIsAnonymous_anonymous() {
+    Authentication anonymousAuth = new AnonymousAuthenticationToken(KEY, "anonymousUser", getAnonymousAuthorities());
     when(context.getAuthentication()).thenReturn(anonymousAuth);
     assertTrue(SpringSecurityELLibrary.isAnonymous());
   }
-  
+
   @Test
-  public void testIsAnonymous_authenticated(){
+  public void testIsAnonymous_authenticated() {
     assertFalse(SpringSecurityELLibrary.isAnonymous());
+  }
+
+  private ArrayList<GrantedAuthority> getAnonymousAuthorities() {
+    ArrayList<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+    authorities.add(new GrantedAuthorityImpl("ROLE_ANONYMOUS"));
+    return authorities;
   }
 }
